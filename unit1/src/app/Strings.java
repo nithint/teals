008ce67fef34b1cee220dffa98296a4a8202d325
Nithin Thomas <nithomas@microsoft.com>
2019-09-06 08:45:02 -0700

    Added class discussion file on strings
    Also added project related files

---
+++ unit1/src/app/Strings.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Strings {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello ");
+        System.out.println("Mrs. Baker\nMr. Soren\nMs. Jonsson\nMr. Bir");
+    }
+}
