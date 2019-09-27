c879eafc2760d2e87702aee694c1a8a458417b6f
Nithin Thomas <nithomas@microsoft.com>
2019-09-27 08:22:34 -0700

    Chapter 2 - Order of operations & string concat

--- unit1/src/app/DataTypes.java
+++ unit1/src/app/DataTypes.java
@@ -2,6 +2,7 @@ package app;
 
 public class DataTypes {
     public static void main(String[] args) throws Exception {
-        System.out.print(7 / 3 * 1.2 + 3 / 2);
+        System.out.println(7 / 3 * 1.2 + 3 / 2);
+        System.out.println(2 + " 2 + 3 " + 4);
     }
 }
---
+++ unit1/src/app/Homework210.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Homework210 {
+    public static void main(String[] args) {
+        int x;
+        System.out.println("x is" x);
+
+        int x = 15.2;
+        System.out.println("x is now + x");
+
+        int y;
+        y = int x + 1;
+        System.out.println("x and y are " + x + and + y);
+    }
+}
